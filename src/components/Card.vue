3a7bd4f959d25f0adf396ed04ffb363e61f25828
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-09-09 14:40:19 +0200

    création page simple + test card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -5,7 +5,7 @@
             <h3 class="text-2xl text-gray-900">{{titre}}</h3>
             <address class="not-italic text-gray-500">{{ lieu }}</address>
             <div class="order-first flex items-center">
-                <div class="text-2xl text-indigo-500">${{ price.toLocaleString("en-US") }}</div>
+                <div class="text-2xl text-indigo-500">${{price.toLocaleString("en-US")}}</div>
                 <div class="text-gray-500 ">/months</div>
             </div>
             <hr class="border-indigo-100 border-t-2 my-2"/>
@@ -13,9 +13,6 @@
                 <div><bed class="inline-block align-top"/>4 Beds </div>
                 <div><bath class="inline-block align-top"/> {{ nbrSDB }} Bathrooms </div>
                 <div><dimension class="inline-block align-top"/> 6x7.5 m² </div>
-            </div>
-            <div class="grid place-items-center w-12 h-12 border-2 border-indigo-100 rounded-full absolute top-7 right-5">
-                <heart :class="{'fill-indigo-500': favoris}"/>
             </div>
         </figcaption>
     </figure>
@@ -38,5 +35,5 @@ defineProps({
         image: String,
         nbrSDB: Number,
         lieu: String,
-    };
+    },
 </script>
