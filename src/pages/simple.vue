674b40d2d472e0142f53131b1232890b92d776b4
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-09-09 15:07:16 +0200

    Card props avec v-bind

--- src/pages/simple.vue
+++ src/pages/simple.vue
@@ -1,11 +1,9 @@
 <template>
 <h1>Page simple</h1>
-<card class="p-4" 
-image="/public/maison1.jpg" 
-titre="Beverly Springfield"
-lieu="2821 Lake Sevilla, Palm Harbor, TX"/>
+<card v-bind="objet"/>
 </template>
 
 <script setup lang="ts">
-    import Card from '@/components/Card.vue';
+    import Card from '../components/Card.vue';
+    const objet = {price:"2000", titre:"Beverly Springfield", image:"/public/maison1.jpg", lieu:"2821 Lake Sevilla, Palm Harbor, TX", nbrSDB:"3", }
 </script>
