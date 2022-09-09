0ff67688a158ffe2029264327b377dd9e9ce06df
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-09-09 14:56:03 +0200

    Page simple avec afficher card maison

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -5,7 +5,7 @@
             <h3 class="text-2xl text-gray-900">{{titre}}</h3>
             <address class="not-italic text-gray-500">{{ lieu }}</address>
             <div class="order-first flex items-center">
-                <div class="text-2xl text-indigo-500">${{price.toLocaleString("en-US")}}</div>
+                <div class="text-2xl text-indigo-500">${{ price.toLocaleString("en-US") }}</div>
                 <div class="text-gray-500 ">/months</div>
             </div>
             <hr class="border-indigo-100 border-t-2 my-2"/>
@@ -14,16 +14,20 @@
                 <div><bath class="inline-block align-top"/> {{ nbrSDB }} Bathrooms </div>
                 <div><dimension class="inline-block align-top"/> 6x7.5 m² </div>
             </div>
+            <div class="grid place-items-center w-12 h-12 border-2 border-indigo-100 rounded-full absolute top-7 right-5">
+                <heart :class="{'fill-indigo-500': favoris}"/>
+            </div>
         </figcaption>
     </figure>
 </template>
 
-<script setup lang="ts">
+<script>
 import bed from "./icones/Bed.vue"
 import bath from "./icones/Bath.vue"
 import dimension from "./icones/Dimension.vue"
 import heart from "./icones/Heart.vue"
-defineProps({
+export default {
+    props: {
         price: {
             type: Number,
             default: 2700,
@@ -36,4 +40,7 @@ defineProps({
         nbrSDB: Number,
         lieu: String,
     },
+    components: { bed, bath, dimension, heart }
+}
+
 </script>
--- src/pages/simple.vue
+++ src/pages/simple.vue
@@ -1,4 +1,12 @@
 <template>
-<h1>test</h1>
+<h1>Page simple</h1>
+import card from "../components/Card.vue"
+<card class="p-4" 
+image="/public/maison1.jpg" 
+titre="Beverly Springfield"
+lieu="2821 Lake Sevilla, Palm Harbor, TX"/>
 </template>
 
+<script setup lang="ts">
+    import Card from '@/components/Card.vue';
+</script>
